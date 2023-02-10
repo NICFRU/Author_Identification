bcb43737d6b695c5624a96a0e275b5fc2f8cf9c1
coreprog <crdhbw@gmail.com>
2023-02-10 10:38:54 +0100

    changes

--- data_prep_for_topic_pred.py
+++ data_prep_for_topic_pred.py
@@ -3,7 +3,7 @@ import pandas as pd
 # %%
 df1 = pd.read_csv("df_tokenized_port_2.csv")
 # %%
-df1 = df1.drop(columns=['Unnamed: 0', 'id', 'gender', 'topic', 'sign', 'date', 'text',
+df1 = df1.drop(columns=['Unnamed: 0', 'id', 'gender', 'age', 'sign', 'date', 'text',
        'lang', 'language_2', 'word_tokenize', 'count_word',
        'count_sent', 'word_tokenize_num_of_stopwords',
        'word_tokenize_without_stopwords',
@@ -20,7 +20,7 @@ df1.sent_tokenize = df1.apply(lambda row: str_list_str(row["sent_tokenize"]), ax
 #%%
 df1 = df1.drop_duplicates()
 # %%
-df1.rename(columns = {'age':'labels','sent_tokenize':'text'}, inplace = True)
+df1.rename(columns = {'topic':'labels','sent_tokenize':'text'}, inplace = True)
 
 #%%
 df1 = df1.drop(index = df1.loc[df1['text'].str.contains("http", na=True)].index).reset_index(drop=True)
@@ -39,11 +39,11 @@ df1.labels.value_counts()
 #%%
 df1.labels.value_counts().sort_index().plot(kind='bar')
 #%%
-df = df1.groupby('labels').head(1150)
+df = df1.groupby('labels').head(160)
 indexlist = df.index
 df = df.reset_index(drop=True)
 # %%
-dfvali = df.groupby('labels').head(1150*0.2)
+dfvali = df.groupby('labels').head(160*0.2)
 # %%
 dftrain = df.drop(index=dfvali.index).reset_index(drop=True)
 dfvali = dfvali.reset_index(drop=True)
@@ -52,8 +52,9 @@ dftest = df1.drop(index=indexlist).reset_index(drop=True)
 #%%
 dftest = dftest.groupby('labels').head(20).reset_index(drop=True)
 # %%
-dftrain.to_csv("train_reg.csv",index=False)
-dfvali.to_csv("vali_reg.csv",index=False)
-dftest.to_csv("test_reg.csv",index=False)
+dftrain.to_csv("train_topic.csv",index=False)
+dfvali.to_csv("vali_topic.csv",index=False)
+dftest.to_csv("test_topic.csv",index=False)
 #%%
-#df1.to_csv("data.csv",index=False)
+len(dftrain.labels.unique())
+# %%
---
+++ huggingface_topicclass.py
@@ -0,0 +1,109 @@
+#%%
+from datasets import load_dataset
+import mlflow
+mlflow.autolog()
+# %%
+
+ds = load_dataset('csv', data_files={'train': 'train_topic.csv', "vali": "vali_topic.csv", 'test': 'test_toipic.csv' })
+
+#%%
+
+#checkpoint = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli"
+#checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+checkpoint = "ProsusAI/finbert"
+#checkpoint = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+#%%
+nl = 40
+#%%
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+#%%
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+#%%
+tokenized_ds = ds.map(preprocess_function, batched=True)
+
+#%%
+from transformers import DataCollatorWithPadding
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+#%%
+from transformers import AutoModelForSequenceClassification
+import torch
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=nl,ignore_mismatched_sizes=True)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(device)
+print(device)
+#%%
+import numpy as np
+from datasets import load_metric
+
+metric = load_metric("accuracy")
+
+#%%
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+#%%
+from transformers import TrainingArguments, Trainer
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_ds["train"],
+    eval_dataset=tokenized_ds["vali"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+
+
+trainer.train()
+#%%
+trainer.evaluate()
+
+#%%
+output = trainer.predict(test_dataset=tokenized_ds["test"] )
+
+#%%
+import torch
+
+tens = torch.from_numpy(output.predictions)
+proctens = torch.softmax(tens, dim=1)*100
+
+predlist= []
+for i in range(0,len(proctens)):
+    predlist.append(torch.Tensor.item((proctens[i] == proctens[i].max()).nonzero(as_tuple=True)[0]))
+
+#%%
+
+from sklearn.metrics import classification_report,accuracy_score,confusion_matrix
+print(classification_report(tokenized_ds["test"]["labels"],predlist,labels=range(0,8)))
+print(accuracy_score(tokenized_ds["test"]["labels"],predlist))
+
+#%%
+import pandas as pd
+clsf_report = pd.DataFrame(classification_report(tokenized_ds["test"]["labels"],predlist,labels=range(0,8),output_dict=True)).transpose()
+clsf_report.to_csv(checkpoint.replace("/","_")+"_clsfreport.csv", index= True)
+
+print(classification_report(tokenized_ds["test"]["labels"],predlist))
